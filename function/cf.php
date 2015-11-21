bf26d617adeb06f4f1385921e78c29bc88a25157
Xi-Plus <huangxuanyuxiplus@gmail.com>
2015-11-21 13:22:31 +0800

    cf change stat

--- function/cf.php
+++ function/cf.php
@@ -6,15 +6,6 @@ class cf {
 	public $pattern="/^[0-9]+[A-Z]{1}$/";
 	private $url='http://codeforces.com/';
 	private $api='http://codeforces.com/api/';
-	private $verdictlist=array(
-		'OK'=>'AC',
-		'WRONG_ANSWER'=>'WA',
-		'TIME_LIMIT_EXCEEDED'=>'TLE',
-		'MEMORY_LIMIT_EXCEEDED'=>'MLE',
-		'RUNTIME_ERROR'=>'RE',
-		'COMPILATION_ERROR'=>'CE',
-		'CHALLENGED'=>'Challenging'
-	);
 
 	public function ojinfo() {
 		$response['name']=$this->name;
@@ -49,6 +40,33 @@ class cf {
 		if (!preg_match($this->pattern, $pid)) return true;
 		else return false;
 	}
+	
+	private $verdictlist=array(
+		'OK'=>'AC',
+		'WRONG_ANSWER'=>'WA',
+		'TIME_LIMIT_EXCEEDED'=>'TLE',
+		'MEMORY_LIMIT_EXCEEDED'=>'MLE',
+		'RUNTIME_ERROR'=>'RE',
+		'COMPILATION_ERROR'=>'CE',
+		'CHALLENGED'=>'Challenging'
+	);
+	
+	private $verdictorder=array(
+		'AC'=>0,
+		'WA'=>1,
+		'TLE'=>2,
+		'MLE'=>3,
+		'RE'=>4,
+		'CE'=>5,
+		'Challenging'=>6
+	);
+
+	private function changestat($statold, $statnew){
+		$statnew=$this->verdictlist[$statnew];
+		if ($statold===null) return $statnew;
+		if ($this->verdictorder[$statold]<$this->verdictorder[$statnew]) return $statold;
+		return $statnew;
+	}
 
 	private function fetch($validtime, $uid) {
 		$data=$this->read($uid);
@@ -58,7 +76,7 @@ class cf {
 		$data=json_decode(cURL_HTTP_Request($this->api.'user.status?handle='.$uid)->html,true)['result'];
 		foreach ($data as $temp) {
 			$pid=$temp['problem']['contestId'].$temp['problem']['index'];
-			$response['stat'][$pid]=$this->verdictlist[$temp['verdict']];
+			$response['stat'][$pid]=$this->changestat($response['stat'][$pid],$temp['verdict']);
 		}
 		$this->save($uid, $response);
 		return $response;
